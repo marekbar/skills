cc0fc6c3992ed8be6231d60c4e51b987098c54a4
marekbar1985 <marekbar1985@gmail.com>
2013-12-13 17:35:58 +0100

    Aktywacja kodem bez starego nunit5

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkillsAndTasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestJednostkowyMaile
+    {
+        [TestMethod]
+        public void MailTest()
+        {
+            Assert.IsTrue(sendEmail("marekbar1985@gmail.com", "Aktywacja", "Kod dostępu"));
+        }
+
+        private bool sendEmail(String email, String title, String message)
+        {
+            var mail = new Gmail();
+
+            mail.FromName = "Aktywacja";
+            mail.From = "skillsandtasks@gmail.com";
+
+            mail.Login = "skillsandtasks";
+            mail.Password = "qqlka123!@";
+
+            mail.Message = message;
+            mail.Subject = "Akctywacja konta";
+
+            mail.MailAddressName = "Ty";
+            mail.MailAddress = email;
+            mail.send();
+
+            return true;
+        }
+    }
+}
