a30a235c8a538841ad45368048ca375dc937225e
marekbar1985 <marekbar1985@gmail.com>
2013-12-14 01:14:10 +0100

    IgnoreExtensionDataObject=false,

--- SkillsAndTasks/MBService.svc.cs
+++ SkillsAndTasks/MBService.svc.cs
@@ -10,6 +10,7 @@ namespace SkillsAndTasks
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehaviorAttribute(IgnoreExtensionDataObject=false)]//as enabled breaks distinct on client side
     public class MBService : IMBService
     {
         marekbarEntities db = new marekbarEntities();
