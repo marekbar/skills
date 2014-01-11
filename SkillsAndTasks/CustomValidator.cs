2775a4e01f12a2c20b0f8a9c10e0b90cbc1f3231
marekbar1985 <marekbar1985@gmail.com>
2014-01-11 12:39:19 +0100

    web service

---
+++ SkillsAndTasks/CustomValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.IdentityModel.Selectors;
+
+namespace SkillsAndTasks
+{
+    public class CustomValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == null || password == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!(userName == "ws1349" && password == "qqlka123"))
+            {
+                throw new FaultException("Password or name is wrong");
+            }
+        }
+    }
+}
